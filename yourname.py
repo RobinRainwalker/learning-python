b78e89113dcac9d712055240188ee87f04138bd4
robinrainwalker <robin.rainwalker@gmail.com>
2018-07-06 13:28:57 -0400

    added a youtba *yourname file

---
+++ yourname.py
@@ -0,0 +1,14 @@
+# name = ''
+# while name != 'your name':
+   # print('name pls')
+   # name = input()
+
+while True:
+    print('name plx')
+    name = input()
+    if name != 'test':
+        continue
+    print('test succesful!')
+    #    break
+# print('noice')
+
